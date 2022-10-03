b0129791842686f7deffde4c4894824181ed1cae
jackonedev <jackone.action.software@gmail.com>
2022-10-03 12:34:38 -0300

    Feature release

--- paquete_proyecto/herramientas/extra.py
+++ paquete_proyecto/herramientas/extra.py
@@ -17,3 +17,45 @@ def check_underscore(df):
         cols[i] = cols[i].replace(" ", "_")
     df.columns = cols
     return df
+
+
+class ColateColumn:
+    """
+    La columna "colate", debe entrar atras que la columna "label". Usamos un pd.reindex()
+    """
+
+    def __init__(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        """Ayuda para cambiar el orden de las columnas:
+
+        Funciona de izquierda a derecha
+
+        ColateColumn(dataset).vos(label_1).aca(label_2)
+
+        label_1 se cuela delante de label_2
+
+        """
+
+        self.dataset = dataset
+
+    def vos(self, colate: str):
+        self.colate = colate
+        return self
+
+    def aca(self, aca: str):
+        # Creo un diccionario que tiene key: label- value: index
+        dicto = {}
+        for i, elemento in enumerate(list(self.dataset.columns)):
+            dicto[elemento] = i
+
+        if dicto[aca] < dicto[self.colate]:
+            for elemento in dicto.keys():
+                if (
+                    dicto[elemento] > dicto[aca]
+                    and dicto[elemento] < dicto[self.colate]
+                ):
+                    dicto[elemento] += 1
+
+            dicto[self.colate] = dicto[aca]
+            dicto[aca] += 1
+            llaves = pd.Series(dicto).sort_values().index
+            return self.dataset.reindex(llaves, axis=1)
--- paquete_proyecto/iniciando/bases.py
+++ paquete_proyecto/iniciando/bases.py
@@ -1,18 +1,29 @@
 import pandas as pd
-from paquete_proyecto.herramientas.extra import str_to_float, check_underscore
+from paquete_proyecto.herramientas.extra import (
+    str_to_float,
+    check_underscore,
+    ColateColumn,
+)
 from paquete_proyecto.herramientas.type_adjust import type_adjust
+
 
 # FUNCION 2
 
 
-def ajustar_tipos(data):
+def ajustes_finales(data):
     """
     Función para ajustar los tipos de variables al formato deseado, de forma manual.
     """
-
-    for i, label in enumerate(list(data.columns)):
+    # Cambiamos el tipo de variable de la columna
+    for label in list(data.columns):
         if label == "Ventas":
             data[label] = data[label].astype(float)
+
+    # Chequeamos que el nombre de las columnas no tenga espacios en blanco " "
+    data = check_underscore(data)
+
+    # Cambiamos el orden de las columnas
+    data = ColateColumn(data).vos("Id").aca("IdCliente")
 
     return data
 
@@ -29,7 +40,6 @@ def importar_databases():
     # Abrimos la base de datos
     file_path = "./id_for_ideas/Ventas.csv"
     ventas = pd.read_csv(file_path, sep=";", low_memory=False)
-    ventas = check_underscore(ventas)
 
     # Aplicamos mascara 1, y separamos con y sin duplicados
     ventas = mascara_1(ventas, "NombreCliente")
@@ -41,13 +51,22 @@ def importar_databases():
         ventas_sin_duplicados, "Cantidad"
     )
 
-    # Creamos el indice para temporal
+    # # Creamos el indice para temporal
     ventas = definir_indice_temporal(ventas, "Fecha")
     ventas_sin_duplicados = definir_indice_temporal(ventas_sin_duplicados, "Fecha")
 
-    # Ajustamos el formato de string para poder ser convertido a float
-    ventas["Ventas"] = str_to_float(ventas["Ventas"])
-    ventas_sin_duplicados["Ventas"] = str_to_float(ventas_sin_duplicados["Ventas"])
+    # Agrego la columna ID para RegEx
+    ventas.reset_index(drop=False, inplace=True)
+    ventas.reset_index(drop=False, inplace=True)
+    ventas.set_index("Fecha", drop=True, inplace=True)
+    ventas.loc[:, "Id"] = ventas["index"] + 1
+    ventas.drop(columns="index", inplace=True)
+
+    ventas_sin_duplicados.reset_index(drop=False, inplace=True)
+    ventas_sin_duplicados.reset_index(drop=False, inplace=True)
+    ventas_sin_duplicados.set_index("Fecha", drop=True, inplace=True)
+    ventas_sin_duplicados.loc[:, "Id"] = ventas_sin_duplicados["index"] + 1
+    ventas_sin_duplicados.drop(columns="index", inplace=True)
 
     return type_adjust(ventas), type_adjust(ventas_sin_duplicados)
 
