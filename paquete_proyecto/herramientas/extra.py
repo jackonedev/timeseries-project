f22711ab741d69309caff35d4b099423095990d0
jackonedev <jackone.action.software@gmail.com>
2022-10-04 15:08:06 -0300

    extra.py: agrego funciones para ajustes finales y definir indice temporal

--- paquete_proyecto/herramientas/extra.py
+++ paquete_proyecto/herramientas/extra.py
@@ -59,3 +59,37 @@ class ColateColumn:
             dicto[aca] += 1
             llaves = pd.Series(dicto).sort_values().index
             return self.dataset.reindex(llaves, axis=1)
+
+
+def definir_indice_temporal(data):
+    """Ayuda para la funcion definir_indice_temporal
+    Cuando se invoca, elige la primera columna que encuentra tipo datetime como índice.
+    Si no encuentra devuelve el objeto original.
+    """
+
+    def chequear_tipo(serie):
+        """Aclaración sobre esta funcion:
+        El formato 'datetime64[ns]' es el estandar que sale de adjust_type().
+
+        Chequear "paquete_proyecto.herramientas.adjust_type"
+        """
+        for i, elemento in enumerate(serie.values):
+            if elemento == "datetime64[ns]":
+                return list(serie.index)[i]
+        return False
+
+    label = chequear_tipo(data.dtypes)
+
+    if label:
+        data[label] = pd.to_datetime(data[label], infer_datetime_format=True)
+        data.sort_values(by=label, inplace=True)
+        return data.set_index(label)
+    return data
+
+
+def setear_tipo(data, label, dtype):
+    try:
+        data[label] = data[label].astype(dtype)
+    except:
+        pass
+    return data
