864ae8a3ded140555f61f9fbf94f65c6b33c6a9d
AgustinSt1990 <jackone.action.software@gmail.com>
2022-10-13 10:38:18 -0300

    actualizaciones 13 de Octubre 11 am

---
+++ paquete_proyecto/data_report/plots.py
@@ -0,0 +1,239 @@
+import pandas as pd
+from matplotlib.pyplot import plot
+import plotly.express as px
+import chart_studio.plotly as py
+import chart_studio.tools as tls
+from time import sleep
+from dotenv import load_dotenv
+import os
+from abc import ABC
+
+from paquete_proyecto.data_report.tools import descargar_imagen
+
+
+class VisualizerData:
+    """VisualizerData: 
+    - Storage some default values for graphs attributes
+    - Storage a list of DataFrames
+    - Storage a list with strings, must have same length than the previous list
+    - Build an iterator for dataframe list
+    - Build an iterator for feature list
+    """
+    height = 400
+    width = 900
+    template = 'plotly_dark'
+    pd.options.plotting.backend = 'plotly'
+
+    def __init__(self, frame_list: list[pd.DataFrame], features: list[str]):
+        self.dataset_iterator = iter(frame_list)
+        self.feature_iterator = iter(features)
+        self.dataframe_list = frame_list
+        self.features = features
+        self.figures = {}
+
+
+class VisualProcessor(ABC, VisualizerData):
+    """ Visual Processor
+    - Tiene su metodo constructivo separado
+    - cumple con la funcion obj.set_content()
+    - admite obj.set_content().set_title()
+    - su objetifo final es obj.set_content().plot(), .plot_area, .plot_kind(kind=)
+    - Contiene un iterador interno y contiene función de selección manual de las features
+    - los parámetros de set_content() son:
+        - feature= None - default: se ejecuta el iterador interno - 'str': si la label existe, se setea esa feature
+        - y_label=None- default: configuración predefinida del graph - 'str': crea el label para el y axis, y el titulo
+        - ascending = False- establece el orden de las columnas de cada DataFrame
+        - title_suffix = None - default: no añade aclaración al titulo - 'str' añade información adicional al titulo
+        - download = False - default: modo de solo viusualización - True: tras cada visualización consulta si se desea descargar la imágen en formato html
+    - los parámetros de set_title() son:
+        - new_title - str: Reemplaza al título que genera el método set_content() por default
+    - El método plot() no tiene parámetros
+    - El método plot_areas() no tiene parámetros
+    - El método plot_kind() recibe los siguientes parámetros:
+        - kind = 'bar' - default: grafica un gráfico de barras - 'str': "bar", "barh", "hist", "box", "line"
+        - resample=None - default: grafica tal cual está el TimeIndex - 'str': "W", "M", "2M", ... admite los parámetros del pd.resample()
+
+    Modo de uso:
+
+    list_feature = ['A', 'B', 'C']
+    list_frames = [pd.DataFrame, pd.DataFrame, pd.DataFrame]
+
+    instance = VisualProcessor(lista_frames, list_feature)
+    for i in range(len(list_feature)):
+        instance.set_content().plot()
+
+    # or Calling a particular feature
+    
+    instance.set_content(feature="B").plot()
+
+    """
+    current_content = None
+    title="{} , {}"
+
+    def _get_content(self):
+        return next(self.dataset_iterator, None)
+
+    def set_title(self, new_title):
+        self.title_format = new_title
+        return self
+
+
+    def set_content(self, feature=None, y_label=None , ascending=False, title_suffix=None, download=False, select_figures=True):
+        """set_content
+        Es un método que devuelve un objeto listo para aplicarsele el método "plot()".
+        Si feature es None ejecuta el iterador
+        Si feature corresponde a alguna feature, si existe, devuelve el dataframe correspondiente procesado
+        """
+        # Collect Dataframe from list by label search
+        if feature is not None and self.features.count(feature) > 0:
+            for i, label in enumerate(self.features):
+                if label == feature:
+                    self.current_content = self.dataframe_list[i]
+                    self.current_feature = feature
+                    break
+        # Collect DataFrame from list by iterator
+        else:
+            if feature is not None:
+                print ("logg. No se encontró la feature solicitada")
+            self.current_content = self._get_content()
+            self.current_feature = next(self.feature_iterator, None)
+        # Ordering columns
+        self.current_content = self.current_content.reindex(dict(zip(self.current_content.sum().sort_values(ascending=ascending).index, self.current_content.columns)), axis=1)
+        # Set graphs attributes for plot        
+        if y_label is not None:
+            self.y_axis_label = {"value": y_label}
+            self.title_format = self.title.format(self.current_feature, y_label)
+        else:
+            self.y_axis_label = {"value": "value"}
+            self.title_format = self.title.format(self.current_feature, "")
+        if title_suffix is not None:
+            self.title_format += f" - ({title_suffix})"
+        # Set user interface parámeters
+        self.download = download
+        self.select_figures = select_figures
+        return self
+
+###############################################
+
+    def plot(self):
+        self.figure = self.current_content.plot(title=self.title_format, height=self.height, width=self.width, labels=self.y_axis_label, template=self.template)
+        sleep(0.1)
+        self.figure.show()
+        if self.download:
+            sleep(0.1)
+            if name := input("Ingrese nombre para guardar\nEnter para salir"):
+                descargar_imagen(obj=self.figure, suffix=name)
+                self.figures[self.current_feature] = self.figure
+                return
+        if self.select_figures:
+            sleep(0.1)
+            if title := input("Ingrese título para guardar\nEnter para saltear"):
+                self.title_format = title
+                self.figure = self.current_content.plot(title=self.title_format, height=self.height, width=self.width, labels=self.y_axis_label, template=self.template)
+                self.figures[self.current_feature] = self.figure
+
+
+    def plot_area(self):
+        self.figure = self.current_content.plot.area(title=self.title_format, height=self.height, width=self.width, labels=self.y_axis_label, template=self.template)
+        sleep(0.1)
+        self.figure.show()
+        if self.download:
+            sleep(0.1)
+            if name := input("Ingrese nombre para guardar\nEnter para salir"):
+                descargar_imagen(obj=self.figure, suffix=name)
+                self.figures[self.current_feature] = self.figure
+                return
+        if self.select_figures:
+            sleep(0.1)
+            if title := input("Ingrese título para guardar\nEnter para saltear"):
+                self.title_format = title
+                self.figure = self.current_content.plot.area(title=self.title_format, height=self.height, width=self.width, labels=self.y_axis_label, template=self.template)
+                self.figures[self.current_feature] = self.figure
+
+
+    def plot_kind(self, kind='bar', resample=None):
+        if resample is not None:
+            self.figure = self.current_content.resample(resample).sum().plot(kind=kind, title=self.title_format, height=self.height, width=self.width, labels=self.y_axis_label, template=self.template)
+        else:
+            self.figure = self.current_content.plot(kind=kind, title=self.title_format, height=self.height, width=self.width, labels=self.y_axis_label, template=self.template)
+        sleep(0.1)
+        self.figure.show()
+        if self.download:
+            sleep(0.1)
+            if name := input("Ingrese nombre para guardar\nEnter para salir"):
+                descargar_imagen(obj=self.figure, suffix=name)
+                self.figures[self.current_feature] = self.figure
+                return
+        if self.select_figures:
+            sleep(0.1)
+            if title := input("Ingrese título para guardar\nEnter para saltear"):
+                self.title_format = title
+                if resample is not None:
+                    self.figure = self.current_content.resample(resample).sum().plot(kind=kind, title=self.title_format, height=self.height, width=self.width, labels=self.y_axis_label, template=self.template)
+                else:
+                    self.figure = self.current_content.plot(kind=kind, title=self.title_format, height=self.height, width=self.width, labels=self.y_axis_label, template=self.template)
+                self.figures[self.current_feature] = self.figure    
+
+    
+
+class GraphUploader:
+    """ GraphUploader
+    - Recieves an VisalProcessor instance as an obj attribute
+    - Shows all the figures plot in the VisualProcesor instance with "show_figures()"
+    - Can set a particular figure with "set_figure()":
+        - if figure is None: set a figure throw input command else provide key for self.figures dictionary
+    - With the "show()" method you can plot your activated figure
+    - With the "upload()" method you can upload plot into Chart Studio, need to provide a "filename"
+    """
+    load_dotenv()
+    USER = os.environ["USER_CS"]
+    TOKEN = os.environ["TOKEN_CS"]
+    tls.set_credentials_file(username=USER, api_key=TOKEN)
+    pd.options.plotting.backend = 'plotly'
+
+    def __init__(self, processor):
+        self.processor: VisualProcessor = processor
+        self.figure = None
+
+    def show_figures(self):
+        print (f"Figures: {self.processor.figures.keys()}")
+
+    def set_figure(self, figure=None, select=False):
+        if figure is not None and list(self.processor.figures.keys()).count(figure) > 0:
+            self.figure = True
+            self.processor.figure = self.processor.figures[figure]
+            return self
+
+        elif select:
+            print (f"Select Figure: {self.processor.figures.keys()}")
+            while True:
+                if name:= input('Select figure = '):
+                    if list(self.processor.figures.keys()).count(name) > 0:
+                        self.figure = True
+                        self.processor.figure = self.processor.figures[name]
+                        return self
+                    else:
+                        print ("label not found: check type writting or press enter to exit")
+                        continue
+                else:
+                    print ("Figure set up failed")
+                break
+        else:
+            self.figure = None
+            return self
+
+        
+
+    def show(self):
+        if self.figure is None:
+            return
+        self.processor.figure.show()
+    
+    def show_all(self):
+        for figure in self.processor.figures.values():
+            if self.figure is None:
+                continue
+            figure.show()
+
+    def upload(self, filename, auto_open=True):
+        py.plot(self.processor.figure, filename=filename, auto_open=auto_open)
---
+++ paquete_proyecto/data_report/tools.py
@@ -0,0 +1,69 @@
+from itertools import count
+import os
+import pickle
+
+
+x1 = count() # for crear_rotulo_html()
+next(x1)
+
+x2 = count() # for crear_rotulo_pickle()
+next(x2)
+
+x3 = count() # for enumerate_figures()
+next(x3)
+
+def enumerar_figuras(func):
+    global x3
+    try:
+        func()
+        print (f"Figura {next(x3)}")
+    except:
+        pass
+
+
+def descargar_imagen(obj, dir_name="html", prefix="img", suffix=None):
+    """Descarga una figure en formato HTML"""
+    obj.write_html(crear_directorio(dir_name) +"\\"+ crear_rotulo_html(prefix=prefix, suffix=suffix), full_html=True)
+
+
+def descargar_objeto(obj, dir_name="objects", prefix="obj", suffix=None):
+    """Descarga un objeto en formato pickle (Serializado)"""
+    file_name = f"{crear_directorio(dir_name)}\\{crear_rotulo_pickle(prefix=prefix, suffix=suffix)}"
+    with open(file_name, 'wb') as f:
+        pickle.dump(obj, f)
+
+
+def cargar_objeto(file_name, dir_name="objects"):
+    """Levanta desde el directorio, un objeto pickle guardado"""
+    file_name = f"{crear_directorio(dir_name)}\\{file_name}"
+    with open(file_name, 'rb') as f:
+        obj = pickle.load(f)
+    return obj
+
+
+def crear_directorio(dir_name):
+    if not dir_name.startswith("\\"):
+        dir_name = "\\" + dir_name
+    path = os.getcwd() + dir_name
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+def string_replace_blank(string):
+    return string.replace(" ", "_")
+
+
+def crear_rotulo_html(prefix, suffix):
+    global x1
+    if suffix is not None:
+        return f"{prefix}_{next(x1)}_{string_replace_blank(suffix)}.html"
+    return f"{prefix}_{next(x1)}.html"
+
+def crear_rotulo_pickle(prefix, suffix):
+    global x2
+    if suffix is not None:
+        return f"{prefix}_{next(x1)}_{string_replace_blank(suffix)}.pickle"
+    return f"{prefix}_{next(x1)}.pickle"
+
+
+
